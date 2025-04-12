82402d83a376ac485d09c0965fd8f95c32028f47
Govind Vijay <gvijay@Govinds-MacBook-Air.local>
2025-04-12 12:17:19 +0530

    added login router and applied styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css'
-import App from './App.tsx'
+import LoginPage from "./pages/LoginPage";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Route for the login page */}
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Placeholder for the chat page (we'll protect it later) */}
+        <Route path="/chat" element={<div>Chat Page Placeholder</div>} />
+
+        {/* Redirect root path to login page initially */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* You might have other routes within App or handle them differently */}
+        {/* Example if App contains other layout: <Route path="/*" element={<App />} /> */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
